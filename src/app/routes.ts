f64a13fb0f58a2dc6f058134e027ebc2a4f42f61
felixcen <felixcen@gmail.com>
2020-04-12 00:17:27 -0400

    Added root guard to protect the routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,27 +3,35 @@ import { HomeComponent } from './home/home.component';
 import { MemberListComponent } from './member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
     {
-        path: 'home',
+        path: '',
         component: HomeComponent
     },
     {
-        path: 'members',
-        component: MemberListComponent
-    },
-    {
-        path: 'lists',
-        component: ListsComponent
-    },
-    {
-        path: 'messages',
-        component: MessagesComponent
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            {
+                path: 'members',
+                component: MemberListComponent
+            },
+            {
+                path: 'lists',
+                component: ListsComponent
+            },
+            {
+                path: 'messages',
+                component: MessagesComponent
+            },
+        ]
     },
     {
         path: '**',
-        redirectTo: 'home',
+        redirectTo: '',
         pathMatch: 'full'
     }
 ];
