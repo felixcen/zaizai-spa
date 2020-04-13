27da022c67083b0d768eb8583476919180d5dddf
felixcen <felixcen@gmail.com>
2020-04-12 21:14:02 -0400

    added member details component implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,17 @@ import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptor, ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { RouterModule } from '@angular/router';
-import { appRoutes } from './routes'; 
+import { appRoutes } from './routes';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtModule } from '@auth0/angular-jwt';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail-resolver';
+import { MemberListResolver } from './_resolvers/member-list-resolver';
 
 export function tokenGetterFromLocalStorage(){
    return localStorage.getItem('token');
@@ -30,6 +34,7 @@ export function tokenGetterFromLocalStorage(){
       RegisterComponent,
       MemberListComponent,
       MemberCardComponent,
+      MemberDetailComponent,
       ListsComponent,
       MessagesComponent
    ],
@@ -39,6 +44,7 @@ export function tokenGetterFromLocalStorage(){
       FormsModule,
       BrowserAnimationsModule,
       BsDropdownModule.forRoot(),
+      TabsModule.forRoot(),
       RouterModule.forRoot(appRoutes),
       JwtModule.forRoot({
          config: {
@@ -51,7 +57,9 @@ export function tokenGetterFromLocalStorage(){
    providers: [
       AuthService,
       ErrorInterceptor,
-      ErrorInterceptorProvider
+      ErrorInterceptorProvider,
+      MemberDetailResolver,
+      MemberListResolver
    ],
    bootstrap: [
       AppComponent
